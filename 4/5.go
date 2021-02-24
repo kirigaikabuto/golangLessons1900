2e974e59c50db4dbd25710a6c0a61d1e41342541
kirigaikabuto <tleugazy98@gmail.com>
2021-02-24 19:51:46 +0600

    sdssd

--- 4/5.go
+++ 4/5.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	//необходимо посчитать сумму цифр 0-20 используя цикл
 	//0+1+2+3+4+5.... =
+	var a, b int
+	fmt.Scanf("%d %d", &a, &b)
+	for i := a; i <= b; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
 }
