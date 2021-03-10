c6cb44ce504dfa07a29c6b086400725b9fd5ac7b
kirigaikabuto <tleugazy98@gmail.com>
2021-03-10 19:43:12 +0600

    SdsddsD

---
+++ 7/2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	sl := []string{"yerassyl", "anel", "aidar", "kairat", "1232"}
+						//0        1        2        3        4
+	a := sl[2:4]
+	fmt.Println(a)
+}
