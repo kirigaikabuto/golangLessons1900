384499e165c1b37d0ca999d5433785a4465e2bc0
kirigaikabuto <tleugazy98@gmail.com>
2021-03-12 19:44:08 +0600

    Feat:add all

--- 8/4.go
+++ 8/4.go
@@ -9,24 +9,29 @@ import (
 //блок кода
 //return то_что_мы_хотим_вернуть_обратно_откуда_вызывали
 //}
-func getMax(a []int) int {
+func getMax(a []int) (int, int) {
 	maxi := a[0]
+	mini := a[0]
 	for i := 0; i < len(a); i++ {
 		if a[i] > maxi {
 			maxi = a[i]
 		}
+		if a[i] < mini {
+			mini = a[i]
+		}
 	}
-	return maxi
+	return maxi, mini
 }
 
 func main() {
 	arr1 := []int{1, 2, 3, 4}
 	arr2 := []int{4, 5, 6, 2, 323, 23, 4, 232}
 	arr3 := []int{1, 23, 4, 123, 23213, 43434, 4545, 12321, 1313213, 343443, 54565}
-	maxi1 := getMax(arr1)
-	maxi2 := getMax(arr2)
-	maxi3 := getMax(arr3)
+	maxi1, mini1 := getMax(arr1)
+	maxi2, mini2 := getMax(arr2)
+	maxi3, mini3 := getMax(arr3)
 	fmt.Println(maxi1, maxi2, maxi3)
-	k := math.Pow(float64(3),float64(2))
+	fmt.Println(mini1, mini2, mini3)
+	k := math.Pow(float64(3), float64(2))
 	fmt.Println(k)
 }
