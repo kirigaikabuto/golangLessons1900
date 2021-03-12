4a8cd121ff32ebcff10e7ed67df0c7d1233910a7
kirigaikabuto <tleugazy98@gmail.com>
2021-03-12 20:27:23 +0600

    Sdsds

---
+++ 8/8.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func replaceElements(a []int) []int {
+	for i := 0; i < len(a); i++ {
+		if a[i]%2 == 0 {
+			a[i] = 0
+		}
+	}
+	return a
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	arr2 := replaceElements(arr)
+	fmt.Println(arr)
+	fmt.Println(arr2)
+}
---
+++ 8/9.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 3, 4}
+	arr1 := arr
+	arr[0] = 123
+	fmt.Println(arr)
+	fmt.Println(arr1)
+}
