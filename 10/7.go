f7904f6a7a41418c0a869b9c62f1bc53d67de47c
Yerassyl <tleugazy98@gmail.com>
2021-03-17 19:20:59 +0600

    sdsd

--- 10/7.go
+++ 10/7.go
@@ -5,13 +5,38 @@ import (
 	"reflect"
 )
 
+//type название_структуры struct {
+//	имя_поля тип_поля(примитивные типы данных или такая же структура)
+//	...
+//}
+//тип данных person у которого есть возраст(age) имя(first_name) фамилия(last_name)
+
+//новый тип данных
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
 func main() {
-	a := 3
-	b := 3.4
-	c := "hello"
-	d := false
-	fmt.Println(reflect.TypeOf(a))
-	fmt.Println(reflect.TypeOf(b))
-	fmt.Println(reflect.TypeOf(c))
-	fmt.Println(reflect.TypeOf(d))
+	//примитивные типы данных
+	//int
+	//float64
+	//string
+	//bool
+
+	//a := 3
+	//b := 3.4
+	//c := "hello"
+	//d := false
+	//fmt.Println(reflect.TypeOf(a))
+	//fmt.Println(reflect.TypeOf(b))
+	//fmt.Println(reflect.TypeOf(c))
+	//fmt.Println(reflect.TypeOf(d))
+	p := Person{"yerassyl", "tleugazy", 23}
+	fmt.Println(p.FirstName)
+	fmt.Println(p.LastName)
+	fmt.Println(p.Age)
+	fmt.Println(reflect.TypeOf(p))
+
 }
