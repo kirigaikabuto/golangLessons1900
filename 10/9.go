6a51becdcf7a4921eb76aca7a065d06334156db0
Yerassyl <tleugazy98@gmail.com>
2021-03-17 19:54:03 +0600

    sdsd

--- 10/9.go
+++ 10/9.go
@@ -7,8 +7,25 @@ type Book struct {
 	Price int
 }
 
-func(b Book) printBook() {
+func (b Book) printBook() {
 	fmt.Println("Название книги:", b.Name, "Цена:", b.Price)
+}
+
+type Library struct {
+	Name    string
+	Address string
+	Books   []Book
+}
+
+func (l Library) getExpensiveBook() {
+	books := l.Books
+	maxi := books[0]
+	for _, v := range books {
+		if v.Price > maxi.Price {
+			maxi = v
+		}
+	}
+	maxi.printBook()
 }
 
 func main() {
@@ -16,26 +33,19 @@ func main() {
 		Name:  "book1",
 		Price: 2,
 	}
-
 	b2 := Book{
 		Name:  "book2",
 		Price: 3,
 	}
-
 	b3 := Book{
 		Name:  "book2",
 		Price: 1,
 	}
-
 	books := []Book{b1, b2, b3}
-	maxi := books[0]
-	for _, v := range books {
-		if v.Price > maxi.Price {
-			maxi = v
-		}
+	l1 := Library{
+		Name:    "Library1",
+		Address: "Library1",
+		Books:   books,
 	}
-	b1.printBook()
-	b2.printBook()
-	b3.printBook()
-	maxi.printBook()
+	l1.getExpensiveBook()
 }
