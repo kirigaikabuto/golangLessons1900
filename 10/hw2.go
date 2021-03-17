f0020a5675ebc5989dac449a116ea3eb1d8c563f
Yerassyl <tleugazy98@gmail.com>
2021-03-17 18:43:57 +0600

    sdsd

---
+++ 10/hw2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 3}
+	n := len(arr)
+	sumi := 0
+	maxi := 0
+	for i := 0; i < n; i++ {
+		if i == n-1 {
+			sumi = arr[i] + arr[0] + arr[1]
+		} else if i == n-2 {
+			sumi = arr[i] + arr[i+1] + arr[0]
+		} else {
+			sumi = arr[i] + arr[i+1] + arr[i+2]
+		}
+		if sumi > maxi {
+			maxi = sumi
+		}
+	}
+	fmt.Println(maxi)
+}
