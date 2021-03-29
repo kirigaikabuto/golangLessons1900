bd36137785d0da0d135dfbf4c2d3edfc82c42e8e
Yerassyl <tleugazy98@gmail.com>
2021-03-29 19:02:57 +0600

    sdsd

--- 11/1.go
+++ 11/1.go
@@ -8,16 +8,21 @@ type Student struct {
 	LastName  string
 	Age       int
 	FullName  string
+	YearBirth int
 }
 
 //methods
 func (st *Student) PrintAll() {
+	//create call of method CreateFullName()
+	//create call of method SetYear()
 	fmt.Println(st.FirstName, st.LastName, st.Age, st.FullName)
 }
 
 func (st *Student) CreateFullName() {
 	st.FullName = st.FirstName + " " + st.LastName
 }
+
+//SetYear() method for calculating year of birth (2021-Age)
 
 func main() {
 	var st1 Student
@@ -26,6 +31,13 @@ func main() {
 		LastName:  "Tleugazy",
 		Age:       23,
 	}
-	st1.CreateFullName()
-	st1.PrintAll()
+	st2 := Student{
+		FirstName: "123231",
+		LastName:  "4123313",
+		Age:       123,
+	}
+	students := []Student{st1, st2}
+	for _, v := range students {
+		v.PrintAll()
+	}
 }
