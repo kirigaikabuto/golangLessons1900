0b2301f28c71cb574ccd1b374da8ee24e4be471a
Yerassyl <tleugazy98@gmail.com>
2021-03-31 19:29:47 +0600

    sdsd

---
+++ 12/1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+}
+
+func main() {
+	p1 := Person{Name: "1"}
+	p2 := Person{Name: "2"}
+	p3 := Person{"3"}
+	persons := []Person{p1, p2, p3}
+	fmt.Println(persons)
+	for i, _ := range persons {
+		persons[i].Name = "123"
+	}
+	fmt.Println(persons)
+}
