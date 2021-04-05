4bb3c7b5cbff03fd9ac38e114174dcf38fffc5b6
Yerassyl <tleugazy98@gmail.com>
2021-04-05 19:02:37 +0600

    sdsd

--- 13/2.go
+++ 13/2.go
@@ -17,6 +17,8 @@ func main() {
 	app.Name = "App for downloading video"
 	app.Description = "Just set the url and video files will bee downloaded"
 	app.Usage = "Set parameter url to link"
+	//функция которая срабатывает автоматически когда запускаем приложение
+	//c *cli.Context -> в ней хранятся все аргументы и флаги которые указываются при запуске приложения
 	app.Action = func(c *cli.Context) error {
 		fmt.Println("hello from cli app")
 		return nil
