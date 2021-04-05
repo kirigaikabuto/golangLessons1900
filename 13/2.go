59fa86e982930710fa8b1ee72346ee6bea66207c
Yerassyl <tleugazy98@gmail.com>
2021-04-05 18:57:00 +0600

    sdsd

--- 13/2.go
+++ 13/2.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
 
+func main() {
+	//создание приложения
+	app := cli.NewApp()
+	//указываем свойства
+	app.Name = "App for downloading video"
+	app.Description = "Just set the url and video files will bee downloaded"
+	app.Usage = "Set parameter url to link"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("hello from cli app")
+		return nil
+	}
+	err := app.Run(os.Args)
+	if err != nil{
+		log.Fatal(err)
+	}
 }
--- 13/go.mod
+++ 13/go.mod
@@ -2,4 +2,7 @@ module github.com/kirigaikabuto/golangLessons1900/13
 
 go 1.13
 
-require github.com/faiface/beep v1.0.2 // indirect
+require (
+	github.com/faiface/beep v1.0.2 // indirect
+	github.com/urfave/cli v1.22.5 // indirect
+)
