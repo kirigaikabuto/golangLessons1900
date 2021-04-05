f6412c0da229897c282eebb5103df67f475b0730
Yerassyl <tleugazy98@gmail.com>
2021-04-05 18:46:24 +0600

    sdsd

--- 13/1.go
+++ 13/1.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"log"
+	"os"
+	"time"
+)
 
+func main() {
+	f, err := os.Open("1.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	st, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer st.Close()
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	speaker.Play(st)
+	select {}
 }
---
+++ 13/go.mod
@@ -0,0 +1,5 @@
+module github.com/kirigaikabuto/golangLessons1900/13
+
+go 1.13
+
+require github.com/faiface/beep v1.0.2 // indirect
