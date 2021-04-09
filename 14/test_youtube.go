01bb490e4006e9395199a9cddd0d22cb1739a5df
Yerassyl <tleugazy98@gmail.com>
2021-04-09 19:00:13 +0600

    sdsd

--- 14/test_youtube.go
+++ 14/test_youtube.go
@@ -9,15 +9,23 @@ var (
 	pathDir = "./videos"
 )
 
-func main() {
-	url := "https://www.youtube.com/watch?v=nmDFmI2oNBY"
+func downloadFromYoutube(url string) error {
 	yt := youtube.NewYoutube(false, true)
 	err := yt.DecodeURL(url)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fileName := yt.Author + "_" + yt.Title + ".mp4"
 	err = yt.StartDownload(pathDir, fileName, "high", 0)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	url := "https://www.youtube.com/watch?v=nmDFmI2oNBY"
+	err := downloadFromYoutube(url)
 	if err != nil {
 		log.Fatal(err)
 	}
