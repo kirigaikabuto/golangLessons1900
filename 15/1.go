30c316d7ac7abb90aa15efcfcdb3b2309199c6a2
Yerassyl <tleugazy98@gmail.com>
2021-04-12 19:19:30 +0600

    sdsd

--- 15/1.go
+++ 15/1.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/kirigaikabuto/golangLessons1900/15/config"
+import (
+	"context"
+	"fmt"
+	"github.com/kirigaikabuto/golangLessons1900/15/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
 
 //go get go.mongodb.org/mongo-driver/mongo
 func main() {
@@ -9,6 +16,23 @@ func main() {
 	cf := config.MongoConfig{
 		Host:     "localhost",
 		Port:     "27017",
-		Database: "test_db_12",
+		Database: "testdb",
 	}
+	//config for client
+	clientOptions := options.Client().ApplyURI("mongodb://" + cf.Host + ":" + cf.Port)
+	//connect to database
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//check for connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//connect to database
+	db := client.Database(cf.Database)
+	//connect to table/collection
+	collection := db.Collection("users")
+	fmt.Println(collection)
 }
