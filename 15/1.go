cce9497dc1715fa4edd2895541e5ef7ca2001305
Yerassyl <tleugazy98@gmail.com>
2021-04-12 19:04:43 +0600

    sdsd

--- 15/1.go
+++ 15/1.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "github.com/kirigaikabuto/golangLessons1900/15/config"
 
+//go get go.mongodb.org/mongo-driver/mongo
+func main() {
+	//connect to database
+	//create config
+	cf := config.MongoConfig{
+		Host:     "localhost",
+		Port:     "27017",
+		Database: "test_db_12",
+	}
 }
