27d2828a9bb187ba815bc957852f059b8b4f42d1
Yerassyl <tleugazy98@gmail.com>
2021-04-30 18:28:57 +0600

    sdsd

--- 21/movie/endpoints.go
+++ 21/movie/endpoints.go
@@ -10,9 +10,9 @@ import (
 )
 
 type HttpEndpoints interface {
-	MainPage(w http.ResponseWriter, r *http.Request)
-	AddPage(w http.ResponseWriter, r *http.Request)
-	AddPageAction(w http.ResponseWriter, r *http.Request)
+	MainPage() func(w http.ResponseWriter, r *http.Request)
+	AddPage() func(w http.ResponseWriter, r *http.Request)
+	AddPageAction() func(w http.ResponseWriter, r *http.Request)
 	DetailPage(idParam string) func(w http.ResponseWriter, r *http.Request)
 }
 
@@ -56,74 +56,80 @@ func (h *httpEndpoints) DetailPage(idParam string) func(w http.ResponseWriter, r
 	}
 }
 
-func (h *httpEndpoints) MainPage(w http.ResponseWriter, r *http.Request) {
-	templateFile, err := template.ParseFiles("templates/main_page.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	movies, err := h.db.List()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	response := &MainPageResponse{Movies: movies}
-	err = templateFile.Execute(w, response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func (h *httpEndpoints) AddPage(w http.ResponseWriter, r *http.Request) {
-	response := &AddPageResponse{Error: ""}
-	templateFile, err := template.ParseFiles("templates/add_page.html")
-	form, err := url.ParseQuery(r.URL.RawQuery)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	errorText := form.Get("error")
-	fmt.Println("error text", errorText)
-	fmt.Println(r.Form.Get("name"))
-	if errorText != "" {
-		response.Error = errorText
-	}
-	err = templateFile.Execute(w, response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+func (h *httpEndpoints) MainPage() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		templateFile, err := template.ParseFiles("templates/main_page.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		movies, err := h.db.List()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		response := &MainPageResponse{Movies: movies}
+		err = templateFile.Execute(w, response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
-func (h *httpEndpoints) AddPageAction(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+func (h *httpEndpoints) AddPage() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := &AddPageResponse{Error: ""}
+		templateFile, err := template.ParseFiles("templates/add_page.html")
+		form, err := url.ParseQuery(r.URL.RawQuery)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		errorText := form.Get("error")
+		fmt.Println("error text", errorText)
+		fmt.Println(r.Form.Get("name"))
+		if errorText != "" {
+			response.Error = errorText
+		}
+		err = templateFile.Execute(w, response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (h *httpEndpoints) AddPageAction() func (w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		name := r.Form.Get("name")
+		description := r.Form.Get("description")
+		imageUrl := r.Form.Get("image_url")
+		errorText := ""
+		if len(name) == 0 || len(description) == 0 || len(imageUrl) == 0 {
+			errorText = "Please write all data"
+			form, _ := url.ParseQuery(r.URL.RawQuery)
+			form.Add("error", errorText)
+			r.URL.RawQuery = form.Encode()
+			http.Redirect(w, r, "/add_page", http.StatusSeeOther)
+			return
+		}
+		movie := &Movie{
+			Name:        name,
+			Description: description,
+			ImageUrl:    imageUrl,
+		}
+		movie, err = h.db.Create(movie)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
-	name := r.Form.Get("name")
-	description := r.Form.Get("description")
-	imageUrl := r.Form.Get("image_url")
-	errorText := ""
-	if len(name) == 0 || len(description) == 0 || len(imageUrl) == 0 {
-		errorText = "Please write all data"
-		form, _ := url.ParseQuery(r.URL.RawQuery)
-		form.Add("error", errorText)
-		r.URL.RawQuery = form.Encode()
-		http.Redirect(w, r, "/add_page", http.StatusSeeOther)
-		return
-	}
-	movie := &Movie{
-		Name:        name,
-		Description: description,
-		ImageUrl:    imageUrl,
-	}
-	movie, err = h.db.Create(movie)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/", http.StatusSeeOther)
-	return
 }
