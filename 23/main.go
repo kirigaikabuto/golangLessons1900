85ead38c3a5e1077a5d07a78264fc70f3feef48e
Yerassyl <tleugazy98@gmail.com>
2021-05-12 19:17:52 +0600

    sdsd

--- 23/main.go
+++ 23/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/kirigaikabuto/golangLessons1900/23/start"
+	"net/http"
+)
 
+func main() {
+	router := mux.NewRouter()
+	mainEndpoints := start.NewHttpEndpoints()
+	router.Methods("GET").Path("/test").HandlerFunc(mainEndpoints.TestEndpoint())
+	fmt.Println("server is running on port 8080")
+	http.ListenAndServe(":8080", router)
 }
