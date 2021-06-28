c87ecc44803b516986c813892d7f8a2b4fdc3b20
Yerassyl <tleugazy98@gmail.com>
2021-06-28 19:23:58 +0600

    sdsd

--- 30/main.go
+++ 30/main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"github.com/djumanoff/amqp"
+	"github.com/kirigaikabuto/golangLessons1900/30/users"
+)
+
 func main() {
+	usersMongoStore, err := users.NewUsersStore(users.MongoConfig{
+		Host:           "localhost",
+		Port:           "27017",
+		Database:       "ivi",
+		CollectionName: "users",
+	})
+	if err != nil {
+		panic(err)
+		return
+	}
+	usersAmqpEndpoints := users.NewUsersAmqpEndpoints(usersMongoStore)
+	rabbitConfig := amqp.Config{
+		Host:     "localhost",
+		Port:     5672,
+		LogLevel: 5,
+	}
+	serverConfig := amqp.ServerConfig{
+		ResponseX: "response",
+		RequestX:  "request",
+	}
+
+	sess := amqp.NewSession(rabbitConfig)
+	err = sess.Connect()
+	if err != nil {
+		panic(err)
+		return
+	}
+	srv, err := sess.Server(serverConfig)
+	if err != nil {
+		panic(err)
+		return
+	}
+	srv.Endpoint("list_users", usersAmqpEndpoints.ListUsers())
+	err = srv.Start()
+	if err != nil {
+		panic(err)
+		return
+	}
 
 }
--- 30/users/amqp_endpoints.go
+++ 30/users/amqp_endpoints.go
@@ -1,2 +1,28 @@
 package users
 
+import (
+	"encoding/json"
+	"github.com/djumanoff/amqp"
+)
+
+type UsersAmqpEndpoints struct {
+	usersStore UsersStore
+}
+
+func NewUsersAmqpEndpoints(u UsersStore) UsersAmqpEndpoints {
+	return UsersAmqpEndpoints{usersStore: u}
+}
+
+func (u *UsersAmqpEndpoints) ListUsers() amqp.Handler {
+	return func(message amqp.Message) *amqp.Message {
+		users, err := u.usersStore.List()
+		if err != nil {
+			panic(err)
+		}
+		dataJson, err := json.Marshal(users)
+		if err != nil {
+			panic(err)
+		}
+		return &amqp.Message{Body: dataJson}
+	}
+}
